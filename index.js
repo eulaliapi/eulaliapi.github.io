34c063f4953860c2b9e4e3f41edb6d75ae4c3b8b
Eulalia Pirone <98905459+eulaliapi@users.noreply.github.com>
2022-10-07 19:02:57 +0200

    active links fixed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+    const homeTop = $('#home').offset().top;
+    const skillsTop = $('#skills').offset().top;
+    const projectsTop = $('#projects').offset().top;
+
     //toggles navbar w < 768px
     $('#button').click(function () {
         $('#navbar').toggleClass('hidden')
@@ -24,41 +28,7 @@ $(document).ready(function () {
         $('.homeRef').removeClass('active-name');
         $(this).addClass('active-bar');
         $('.navRef').not(this).removeClass('active-bar');
+
     })
-
-    //toggles active state while scrolling page
-    $(window).scroll(function () {
-        if (this.scrollY < 460) {
-            $('.homeRef').addClass('active-name');
-            $('.navRef').removeClass('active-bar');
-        } else if (this.scrollY > 460 && this.scrollY < 950) {
-
-            $('.homeRef').removeClass('active-name');
-            if ($('.navRef').eq(0).text() === 'Skills') {
-                $('.navRef').eq(0).addClass('active-bar');
-                $('.navRef').eq(1).removeClass('active-bar');
-                $('.navRef').eq(2).removeClass('active-bar');
-            }
-
-        } else if (this.scrollY > 950 && this.scrollY < 1300) {
-
-            $('.homeRef').removeClass('active-name');
-            if ($('.navRef').eq(1).text() === 'Projects') {
-                $('.navRef').eq(0).removeClass('active-bar');
-                $('.navRef').eq(1).addClass('active-bar');
-                $('.navRef').eq(2).removeClass('active-bar');
-            }
-
-        } else if (this.scrollY > 1300) {
-
-            $('.homeRef').removeClass('active-name');
-            if ($('.navRef').eq(2).text() === 'Contact me') {
-                $('.navRef').eq(0).removeClass('active-bar');
-                $('.navRef').eq(1).removeClass('active-bar');
-                $('.navRef').eq(2).addClass('active-bar');
-            }
-        }
-
-    });
 
 });
