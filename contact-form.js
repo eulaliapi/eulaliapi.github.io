958e652763d2e8a5b38fff5a007c0a257d8bda7b
Eulalia Pirone <98905459+eulaliapi@users.noreply.github.com>
2022-12-07 10:16:23 +0100

    update

--- contact-form.js
+++ contact-form.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     //toggles dark/light mode
     $('#moon').click(function () {
-        $(this).toggleClass('active-btn');
         $('html').toggleClass('dark');
         $('.name').toggleClass('dark');
         $('.navRef').toggleClass('dark');
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 
     //toggles dark/light mode
     $('#moon').click(function () {
-        $(this).toggleClass('active-btn');
         $('html').toggleClass('dark');
         $('.name').toggleClass('dark');
         $('.navRef').toggleClass('dark');
